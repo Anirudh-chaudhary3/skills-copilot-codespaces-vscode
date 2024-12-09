22b842fc80fe423ef7feb4a01a11b81f72d0e320
ANIRUDH CHAUDHARY <168189044+Anirudh-chaudhary3@users.noreply.github.com>
2024-12-09 02:43:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var skills = {
+        html: 10,
+        css: 10,
+        javascript: 10,
+        php: 10,
+        python: 10,
+        ruby: 10,
+        java: 10,
+        sql: 10
+    };
+    return skills;
+}
